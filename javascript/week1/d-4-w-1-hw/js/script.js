aec2ebfafe910f837dd24a701ac31573f2e9e3b8
abdurrahman2010 <161490888+abdurrahman2010@users.noreply.github.com>
2024-11-06 21:12:36 +0500

    Add files via upload

---
+++ javascript/week1/d-4-w-1-hw/js/script.js
@@ -0,0 +1,10 @@
+// Пайдаланушыдан үш санды енгізуін сұрау
+let firstNumber = parseFloat(prompt("Бірінші санды енгізіңіз:"));
+let secondNumber = parseFloat(prompt("Екінші санды енгізіңіз:"));
+let thirdNumber = parseFloat(prompt("Үшінші санды енгізіңіз:"));
+
+// Орташа арифметикасын формула арқылы есептеу
+let average = (firstNumber + secondNumber + thirdNumber) / 3;
+
+// Нәтижені alert функциясы арқылы көрсетуyy
+alert("Енгізілген сандардың орташа арифметикалық мәні: " + average);
---
+++ javascript/week2/js-w2-d3/script.js
@@ -0,0 +1,14 @@
+// let age = +prompt("баганызды енгыныз");
+
+// if (age >= 90 && age <= 100) {
+//   alert("a");
+// } else if (age >= 80 && age <= 89) {
+//   alert("B");
+// } else if (age >= 70 && age <= 79) {
+//   alert("C");
+// } else if (age >= 50 && age <= 69) {
+//
+
+
+
+
